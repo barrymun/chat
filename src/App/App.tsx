79163a5d98145d923def3d6b3f7ab5e3df9120ab
Neil <barrymun@tcd.ie>
2022-11-18 11:32:24 +0000

    refactoring app, install sass

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {useState} from 'react'
+import reactLogo from '../assets/react.svg'
+import classes from './App.module.scss';
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div >
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/public/vite.svg" className={classes.logo} alt="Vite logo"/>
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className={`${classes.logo} ${classes.react}`} alt="React logo"/>
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className={classes.card}>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -24,11 +24,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className={classes.readTheDocs}>
         Click on the Vite and React logos to learn more
       </p>
     </div>
   )
 }
-
-export default App
