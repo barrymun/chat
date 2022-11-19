1ec337d4a4ac339a48a6e6978cd90f872dd7bbaf
Neil <barrymun@tcd.ie>
2022-11-19 14:11:37 +0000

    creating messages

--- src/components/Chat/ChatRoom/ChatRoom.tsx
+++ src/components/Chat/ChatRoom/ChatRoom.tsx
@@ -1,8 +1,8 @@
 import classes from "./ChatRoom.module.scss";
 import {ChangeEvent, FormEvent, useEffect, useState} from "react";
-import {collection, query, getDocs, orderBy, limit} from "firebase/firestore";
+import {collection, query, getDocs, orderBy, limit, doc, setDoc, Timestamp, addDoc} from "firebase/firestore";
 import {CollectionReference, Query, QuerySnapshot} from "@firebase/firestore"
-import {firestore, DOCUMENTS_LIMIT, COLLECTION_MESSAGE, ORDER_BY_FIELD_PATH_DEFAULT} from "common/constants";
+import {firestore, DOCUMENTS_LIMIT, COLLECTION_MESSAGE, ORDER_BY_FIELD_PATH_DEFAULT, auth} from "common/constants";
 import SignOut from "components/SignOut";
 import ChatMessage from "components/Chat/ChatMessage";
 
@@ -16,21 +16,31 @@ export default function ChatRoom() {
 
   useEffect(() => {
     async function fetch() {
-      let messages: Array<any> = [];
-      const querySnapshot: QuerySnapshot = await getDocs(q);
-      // ensure to reverse the messages so that they appear correctly on the page
-      querySnapshot.forEach((doc) =>
-        messages = [{...doc.data(), id: doc.id}, ...messages]);
-      setMessages(messages);
-      console.log(messages)
+      await getMessages();
     }
 
     fetch();
   }, []);
 
+  const getMessages = async (): Promise<void> => {
+    let messages: Array<any> = [];
+    const querySnapshot: QuerySnapshot = await getDocs(q);
+    // ensure to reverse the messages so that they appear correctly on the page
+    querySnapshot.forEach((doc) =>
+      messages = [{...doc.data(), id: doc.id}, ...messages]);
+    setMessages(messages);
+  };
+
   const sendMessage = async (e: FormEvent) => {
     e.preventDefault();
-    console.log('form_HERE')
+    const docData: object = {
+      uid: auth.currentUser?.uid,
+      text: formMessage,
+      createdAt: Timestamp.now(),
+    };
+    await addDoc(messagesRef, docData);
+    await getMessages();
+    setFormMessage('');
   };
 
   return <>
@@ -43,7 +53,7 @@ export default function ChatRoom() {
     <form className={classes.form} onSubmit={sendMessage}>
       <input className={classes.formInput} autoFocus value={formMessage}
              onChange={(e: ChangeEvent<HTMLInputElement>) => setFormMessage(e.target.value)}/>
-      <button className={classes.formBtn} type="submit">Send</button>
+      <button className={classes.formBtn} disabled={formMessage === ''} type="submit">Send</button>
     </form>
   </>;
 }
