0b625922329dc85b49c63b4edd82cd84fd1ef3ec
Neil <barrymun@tcd.ie>
2022-11-19 13:48:34 +0000

    send button with input

--- src/components/Chat/ChatRoom/ChatRoom.tsx
+++ src/components/Chat/ChatRoom/ChatRoom.tsx
@@ -1,5 +1,5 @@
 import classes from "./ChatRoom.module.scss";
-import {useEffect, useState} from "react";
+import {ChangeEvent, FormEvent, useEffect, useState} from "react";
 import {collection, query, getDocs, orderBy, limit} from "firebase/firestore";
 import {CollectionReference, Query, QuerySnapshot} from "@firebase/firestore"
 import {firestore, DOCUMENTS_LIMIT, COLLECTION_MESSAGE, ORDER_BY_FIELD_PATH_DEFAULT} from "common/constants";
@@ -9,6 +9,7 @@ import ChatMessage from "components/Chat/ChatMessage";
 export default function ChatRoom() {
 
   const [messages, setMessages] = useState<Array<any>>([]);  // TODO: change "any"
+  const [formMessage, setFormMessage] = useState<string>('');
   const messagesRef: CollectionReference = collection(firestore, COLLECTION_MESSAGE);
   // get the most recent messages given the limit
   const q: Query = query(messagesRef, orderBy(ORDER_BY_FIELD_PATH_DEFAULT, "desc"), limit(DOCUMENTS_LIMIT));
@@ -27,11 +28,22 @@ export default function ChatRoom() {
     fetch();
   }, []);
 
+  const sendMessage = async (e: FormEvent) => {
+    e.preventDefault();
+    console.log('form_HERE')
+  };
+
   return <>
     <SignOut/>
 
     <div className={classes.messages}>
       {messages.map((message, index) => <ChatMessage key={index} message={message}/>)}
     </div>
+
+    <form className={classes.form} onSubmit={sendMessage}>
+      <input className={classes.formInput} autoFocus value={formMessage}
+             onChange={(e: ChangeEvent<HTMLInputElement>) => setFormMessage(e.target.value)}/>
+      <button className={classes.formBtn} type="submit">Send</button>
+    </form>
   </>;
 }
