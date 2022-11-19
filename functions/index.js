06f9f6a13d91c3f2d380f229bac38c35c7158cb2
Neil <barrymun@tcd.ie>
2022-11-19 22:20:47 +0000

    [fix] function (eslint issues)

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,17 @@
 const functions = require("firebase-functions");
 const Filter = require("bad-words");
 const admin = require("firebase-admin");
-
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 admin.initializeApp();
 
-const db = admin.firestore;
-exports.detectBadUsers = functions.firestore
-  .document('message/{msgId}')
-  .onCreate(async (doc, ctx) => {
-    const filter = new Filter();
-    const {text, uid} = doc.data();
+// const db = admin.firestore();
 
-    if (filter.isProfane(text)) {
-      const cleaned = filter.clean(text);
-      await doc.ref.update({text: cleaned});
-    }
-  });
+exports.detectBadUsers = functions.firestore
+    .document("/message/{msgId}")
+    .onCreate((snap, ctx) => {
+      const filter = new Filter();
+      const {text} = snap.data();
+      if (filter.isProfane(text)) {
+        const cleaned = filter.clean(text);
+        return snap.ref.update({text: cleaned});
+      }
+    });
