e0b8ceb6a09572ec5f23ea74b0f3344174d3b2e9
Neil <barrymun@tcd.ie>
2022-11-19 14:17:45 +0000

    utilising onSnapshot for real-time updates

--- src/components/Chat/ChatRoom/ChatRoom.tsx
+++ src/components/Chat/ChatRoom/ChatRoom.tsx
@@ -1,8 +1,8 @@
 import classes from "./ChatRoom.module.scss";
-import {ChangeEvent, FormEvent, useEffect, useState} from "react";
-import {collection, query, getDocs, orderBy, limit, doc, setDoc, Timestamp, addDoc} from "firebase/firestore";
-import {CollectionReference, Query, QuerySnapshot} from "@firebase/firestore"
-import {firestore, DOCUMENTS_LIMIT, COLLECTION_MESSAGE, ORDER_BY_FIELD_PATH_DEFAULT, auth} from "common/constants";
+import {ChangeEvent, FormEvent, useState} from "react";
+import {addDoc, collection, limit, onSnapshot, orderBy, query, Timestamp} from "firebase/firestore";
+import {CollectionReference, Query} from "@firebase/firestore"
+import {auth, COLLECTION_MESSAGE, DOCUMENTS_LIMIT, firestore, ORDER_BY_FIELD_PATH_DEFAULT} from "common/constants";
 import SignOut from "components/SignOut";
 import ChatMessage from "components/Chat/ChatMessage";
 
@@ -13,34 +13,40 @@ export default function ChatRoom() {
   const messagesRef: CollectionReference = collection(firestore, COLLECTION_MESSAGE);
   // get the most recent messages given the limit
   const q: Query = query(messagesRef, orderBy(ORDER_BY_FIELD_PATH_DEFAULT, "desc"), limit(DOCUMENTS_LIMIT));
-
-  useEffect(() => {
-    async function fetch() {
-      await getMessages();
-    }
-
-    fetch();
-  }, []);
-
-  const getMessages = async (): Promise<void> => {
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {
     let messages: Array<any> = [];
-    const querySnapshot: QuerySnapshot = await getDocs(q);
-    // ensure to reverse the messages so that they appear correctly on the page
     querySnapshot.forEach((doc) =>
       messages = [{...doc.data(), id: doc.id}, ...messages]);
     setMessages(messages);
-  };
+  });
+
+  // useEffect(() => {
+  //   async function fetch() {
+  //     await getMessages();
+  //   }
+  //
+  //   fetch();
+  // }, []);
+
+  // const getMessages = async (): Promise<void> => {
+  //   let messages: Array<any> = [];
+  //   const querySnapshot: QuerySnapshot = await getDocs(q);
+  //   // ensure to reverse the messages so that they appear correctly on the page
+  //   querySnapshot.forEach((doc) =>
+  //     messages = [{...doc.data(), id: doc.id}, ...messages]);
+  //   setMessages(messages);
+  // };
 
   const sendMessage = async (e: FormEvent) => {
     e.preventDefault();
+    setFormMessage('');
+    // TODO: add another boolean value to disable button use if a message hasn't been created yet?
     const docData: object = {
       uid: auth.currentUser?.uid,
       text: formMessage,
       createdAt: Timestamp.now(),
     };
     await addDoc(messagesRef, docData);
-    await getMessages();
-    setFormMessage('');
   };
 
   return <>
