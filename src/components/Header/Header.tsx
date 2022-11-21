3858108727b85ad75057b97669ac68bdc1de440b
Neil <barrymun@tcd.ie>
2022-11-21 10:27:33 +0000

    [fix] overflow issue (padding was added to the flex container instead of items within the container)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,13 +8,15 @@ interface Props {
 
 export default function Header({photoURL}: Props) {
   return <>
-    <div className={classes.container}>
+    <div className={`${classes.item} ${classes.container}`}>
 
       <figure className={classes.figure}>
-        <img alt="profile picture" referrerPolicy="no-referrer" className={classes.profile} src={photoURL} />
+        <img alt="profile picture" referrerPolicy="no-referrer" className={classes.profile} src={photoURL}/>
       </figure>
 
-      <SignOut/>
+      <span className={classes.item}>
+        <SignOut/>
+      </span>
 
     </div>
   </>;
